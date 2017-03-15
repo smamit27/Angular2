2f267e72b9f75f4a65a355d3a220ff5d165673ef
Amit Singh <smamit27@gmail.com>
2017-03-15 22:18:21 +0530

    call one components call another components

--- crud/src/app/crud.component.ts
+++ crud/src/app/crud.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/index';
+
+
 
 @Component({
   moduleId: module.id,
   selector: 'crud-app',
   templateUrl: 'crud.component.html',
-  styleUrls: ['crud.component.css']
+  styleUrls: ['crud.component.css'],
+  directives: [HomeComponent]
 })
 export class CrudAppComponent {
   title = 'crud works!';
+  name = 'Crud Operation Start';
 }
